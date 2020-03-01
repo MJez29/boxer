8a56c3a9462a65b06dfb600f09173cd1180953f5
Michal Jez <Michal.G.Jez@gmail.com>
2020-03-01 11:41:05 -0500

    Update about page

---
+++ src/options/components/Code.svelte
@@ -0,0 +1,12 @@
+<style>
+  code {
+    background-color: rgba(41, 4, 11, 0.1);
+    padding-left: 5px;
+    padding-right: 5px;
+    border-radius: 4px;
+  }
+</style>
+
+<code>
+  <slot />
+</code>
--- src/options/sections/About.svelte
+++ src/options/sections/About.svelte
@@ -1,2 +1,14 @@
-<h1>Boxer</h1>
-<p>Boxer is a browser extension to customize your search bar experience.</p>
+<script>
+  import Header from "../components/Header";
+  import Code from "../components/Code";
+</script>
+
+<Header>Boxer</Header>
+<p>
+  Boxer is a browser extension that lets you customize your search bar
+  experience. To activate Boxer simple type
+  <Code>/</Code>
+  in your search bar followed by a space or a tab. For Boxer to be of assistance
+  you must first define aliases. An alias is a mapping from a series of
+  keywords, for example "google maps", to a url, "https://maps.google.com".
+</p>
