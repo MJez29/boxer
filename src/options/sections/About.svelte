86fdcfca29a44b1af74d63946ac8f7510d97e40b
Michal Jez <Michal.G.Jez@gmail.com>
2020-03-05 01:03:52 -0500

    Add more about copy

--- src/options/sections/About.svelte
+++ src/options/sections/About.svelte
@@ -12,3 +12,22 @@
   you must first define aliases. An alias is a mapping from a series of
   keywords, for example "google maps", to a url, "https://maps.google.com".
 </p>
+
+<Header>Adding an Alias</Header>
+<p>
+  To add an alias simply navigate to the Aliases page and enter a name and link
+  for your alias in the empty row. Once you are satisfied with your alias press
+  the checkmark to save!
+</p>
+
+<Header>Activating Boxer in the Search Bar</Header>
+<p>
+  In the search bar enter a forward slash (
+  <Code>/</Code>
+  ) followed by either the tab key or the space key. This activates Boxer. Now
+  start typing the name of the alias you creating in the last section, it should
+  appear as the first search result. You can choose it by clicking on it, by
+  tabbing down to it and pressing enter, or by clicking enter and having Boxer
+  take you to the link that best matches your input (in this case it should be
+  your alias).
+</p>
